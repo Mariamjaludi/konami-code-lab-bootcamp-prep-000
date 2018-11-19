873039c24ab4bfe0c412ed592cf5ce5e54ce545c
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-19 19:42:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,20 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  //your code here
+let i = 0;
+  function onKeyDownHandler(e) {
+      const key = e.key;
+      if (key === codes[i]) {
+        i++;
+
+        if (i === codes.length) {
+          alert("Konami!");
+          i = 0;
+        }
+      } else {
+        i = 0;
+      }
+    }
+
 }
